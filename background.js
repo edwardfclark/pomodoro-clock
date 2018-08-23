8bdebe29db728f0e792bb9af09c31a4b5ba36c1f
Edward Clark <edwardfclark@gmail.com>
2018-08-23 16:32:45 +0100

    I had to nuke this from orbit.
    I couldn't get my background.js file to change items on the DOM of my
    popup. This means I had to change the whole structure of the app. What
    I'm going to try instead is using the background.js file to store the
    variables and the functions used to change those variables, which I can
    confirm works, and handle all DOM manipulation inside timer-script.js.

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
-    let timer = setTimeout(() => alert("Yep, this worked."), 10000);
+    //let testTimer = setTimeout(() => alert("Yep, this refreshed."), 10000);
 });
 
+let timer;
+let bgTestVar = "Loading...";
+
+function timerTest() {
+    timer = setTimeout(function() {
+        bgTestVar = "Success!";
+        console.log(bgTestVar);
+    }, 5000);
+}
+
+function getter() {
+    return bgTestVar;
+}
